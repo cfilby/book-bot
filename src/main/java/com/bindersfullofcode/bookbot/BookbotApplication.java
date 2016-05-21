8fde5828a617f417d4379bef585004ad56c6b6b7
Cooper Filby <cfilby@uab.edu>
2016-05-20 20:29:47 -0500

    Add first commit

---
+++ src/main/java/com/bindersfullofcode/bookbot/BookbotApplication.java
@@ -0,0 +1,12 @@
+package com.bindersfullofcode.bookbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookbotApplication.class, args);
+	}
+}
